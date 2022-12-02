8b2faf7fbc8d60809c04c5ee67fc40d97fa14acf
Pedro <ufcmipedro@alu.ufc.br>
2022-12-02 10:49:54 -0300

    adicionando função próxima chave da fila

--- FIFO/fila_fifo.c
+++ FIFO/fila_fifo.c
@@ -11,26 +11,38 @@ return;
 }
 
 int f_inserir(Fila_FIFO **f, int chave, int valor) {
+Fila_FIFO *fila = f[0];
 Noh *novo;
 novo = (Noh *)malloc(sizeof(Noh));
 if (novo == NULL) return 1; // erro ao acessar a memoria
 novo->chave = chave;
 novo->valor = valor;
 novo->prox = NULL;
-if (f[0]->primeiro == NULL){ // fila Vazia
+if (fila->primeiro == NULL){ // fila Vazia
     // O primeiro e o ultimo apontaram pro mesmo valor
-    f[0]->primeiro = novo;
-    f[0]->ultimo = novo;
+    fila->primeiro = novo;
+    fila->ultimo = novo;
 } else { // Fila nao vazia
     // penúltimo aponta pro novo ultimo
-    f[0]->ultimo->prox = novo;
+    fila->ultimo->prox = novo;
     // novo Noh vai ser o ultimo da fila
-    f[0]->ultimo = novo;
+    fila->ultimo = novo;
 }
 return 0;
 }
 
-int f_obter_proxima_chave(Fila_FIFO **f) { return 0; }
+int f_obter_proxima_chave(Fila_FIFO **f) {
+int chave;
+Fila_FIFO *fila = f[0];
+if (fila->primeiro == NULL) return -1; // Fila Vazia
+chave = fila->primeiro->chave;
+Noh *temp = fila->primeiro;
+// fila tem proximo valor
+if (fila->primeiro->prox != NULL)
+    fila->primeiro = fila->primeiro->prox;
+free(temp);
+return chave;
+}
 
 int f_consultar_proxima_chave(Fila_FIFO **f) { return 0; }
 
