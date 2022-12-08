57b8d5c9634b73a44b90a15f42015c78ce8fe1f2
Pedro Igor <ufcmipedro@alu.ufc.br>
2022-12-08 19:09:24 -0300

    terminando o cliente

--- Cliente/cliente.c
+++ Cliente/cliente.c
@@ -2,31 +2,44 @@
 
 #include <stdlib.h>
 
-int pegarClasse(char *linha) {
-  char *Classe;
-  size_t i;
-  int count;
-  while (linha != "-") count++;
-  Classe = (char *)malloc(count + 1);
-  for (i = 0; i < count; i++) Classe[i] = linha[i];
-  Classe[i] = '\0';
-  if (Classe == "Premiun") return 1;
-  if (Classe == "Ouro") return 2;
-  if (Classe == "Prata") return 3;
-  if (Classe == "Bronze") return 4;
-  if (Classe == "Leezu") return 5;
+int Classe(char *Classe) {
+  if (strcmp("Premiun", Classe) == 0) return 1;
+  if (strcmp("Ouro", Classe) == 0) return 2;
+  if (strcmp("Prata", Classe) == 0) return 3;
+  if (strcmp("Bronze", Classe) == 0) return 4;
+  if (strcmp("Leezu", Classe) == 0) return 5;
   return 0;
 }
 
-int pegarConta(char *linha) {}
+int lidarCliente(char *linha, Cli *cliente) {
+  char *temp;
+  size_t i, j;
+  temp = (char *)malloc(10);
+  if (temp == NULL) return 0;
 
-int pegarQntOpe(char *linha) {}
+  // Este fragmento vai ler a classe
+  for (j = 0, i = 0; linha[i] != ' '; i++, j++) temp[j] = linha[i];
+  j++, temp[j + 1] = '\0';
+  i = i + 3;
+  cliente->classe = Classe(temp);
+
+  // Esta fragmento vai ler a conta
+  for (j = 0; linha[i] != ' '; i++) temp[j] = linha[i];
+  for (j = 0; linha[i] != ' '; i++) temp[j] = linha[i];
+  j++, temp[j + 1] = '\0';
+  cliente->conta = atoi(temp);
+
+  // este fragmente vai lidar com o numero de operação
+  for (i = i + 2, j = 0; linha[i] != ' '; i++) temp[j] = linha[i];
+  temp[i] = '\0';
+  cliente->ops = atoi(temp);
+  free(temp);
+  return 1;
+}
 
 Cli *criarCliente(char *linha) {
   Cli *novo;
   novo = (Cli *)malloc(sizeof(Cli));
-  novo->classe = pegarClasse(linha);
-  novo->conta = pegarConta(linha);
-  novo->ops = pegarQntOpe(linha);
+  lidarCliente(linha, novo);
   return novo;
 }
