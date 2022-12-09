55ab504d9a8d30bb745d1ffa7c3bc99cb37e8728
Pedro Igor <ufcmipedro@alu.ufc.br>
2022-12-08 22:35:42 -0300

    adicionado as funçoes da fila

--- FIFO/fila_fifo.c
+++ FIFO/fila_fifo.c
@@ -2,57 +2,115 @@
 
 #include <stdlib.h>
 
-/*Função que inicia a Fila e
-aloca um tipo Fila_FIFO Vazia ao vetor.*/
-void f_inicializar(Fila_FIFO **f) {
-  Fila_FIFO *novo;
-  novo = (Fila_FIFO *)malloc(sizeof(Fila_FIFO));
-  novo->primeiro = NULL;
-  novo->ultimo = NULL;
-  return;
-}
+// inicializando a fila deixando o ponteiro para ela nulo
+void f_inicializar(Fila_FIFO **f) { *f = NULL; }
 
+// funcao para inserir na fila
 int f_inserir(Fila_FIFO **f, int chave, int valor) {
-  Noh *novo;
-  novo = (Noh *)malloc(sizeof(Noh));
+  // inicializando o noh a ser inserido
+  Noh *novo = (Noh *)malloc(sizeof(Noh));
+  if (novo == NULL) return 0;  // falta de memoria
 
-  if (novo == NULL) return 1;  // erro ao acessar a memoria
-
+  // atribuindo os valores do noh
   novo->chave = chave;
   novo->valor = valor;
   novo->prox = NULL;
 
-  if ((*f)->primeiro == NULL) {  // fila Vazia
-    // O primeiro e o ultimo apontaram pro mesmo valor
-    (*f)->primeiro = novo;
-    (*f)->ultimo = novo;
-  } else {  // Fila nao vazia
-    // penúltimo aponta pro novo ultimo
-    (*f)->ultimo->prox = novo;
-    // novo Noh vai ser o ultimo da fila
-    (*f)->ultimo = novo;
+  // caso em que a fila esta vazia
+  if (*f == NULL) {
+    // inicio ela com o noh 'novo'
+    *f = (Fila_FIFO *)malloc(sizeof(Fila_FIFO));
+    if (*f == NULL) return 0;  // falta de memoria
+    (*f)->fim = novo;
+    (*f)->inicio = novo;
+    (*f)->tam = 1;
+    return 1;
+    // retorno 1 mostrando sucesso na operacao
   }
 
-  return 0;
+  // percorro a fila para ver se ha alguma chave com o mesmo valor do parametro
+  for (Noh *p = (*f)->inicio; p != NULL; p = p->prox)
+    if (p->chave == chave) return 0;  // duplicacao de chave
+
+  // se nao houver, adiciono o noh na fila e atualizo o tamanho da mesma
+  (*f)->fim->prox = novo;
+  (*f)->fim = (*f)->fim->prox;
+  (*f)->tam++;
+
+  return 1;
+  // inidico que a insercao foi bem sucedida
 }
 
+// funcao para obter a proxima chave
 int f_obter_proxima_chave(Fila_FIFO **f) {
-  int chave;
-  if ((*f)->primeiro == NULL) return -1;  // Fila Vazia
-  chave = (*f)->primeiro->chave;
-  Noh *temp = (*f)->primeiro;
-  // fila tem proximo valor
-  if ((*f)->primeiro->prox != NULL) (*f)->primeiro = (*f)->primeiro->prox;
-  free(temp);
+  // se a fila estiver vazia retorno -1 indicando erro
+  if (*f == NULL) return -1;
+
+  // se nao pego o noh que representa o inicio da fila e seu valor chave
+  Noh *aux = (*f)->inicio;
+  int chave = aux->chave;
+  (*f)->inicio = (*f)->inicio->prox;
+  (*f)->tam--;
+  // retiro o noh que estava no inicio e atualizo a fila
+
+  // se o inicio da fila ficar nulo, inicializo ela
+  if ((*f)->inicio == NULL) f_inicializar(f);
+
+  // deleto da memoria o noh que estava no inicio
+  free(aux);
+
   return chave;
+  // retorno a chave do noh que estava no inicio
 }
 
-int f_consultar_proxima_chave(Fila_FIFO **f) { return 0; }
+// funcao para consultar a proxima chave, sem retirar o noh
+int f_consultar_proxima_chave(Fila_FIFO **f) {
+  return (*f)->inicio->chave;
+  // retorno o valor da chave do inicio da fila
+}
 
-int f_consultar_proximo_valor(Fila_FIFO **f) { return 0; }
+// funcao para consultar o proximo valor, sem retirar o noh
+int f_consultar_proximo_valor(Fila_FIFO **f) {
+  return (*f)->inicio->valor;
+  // retorno o valor do inicio da fila
+}
 
-int f_num_elementos(Fila_FIFO **f) { return 0; }
+// funcao que retorna o tamanho de uma fila
+int f_num_elementos(Fila_FIFO **f) {
+  return (*f)->tam;
+  // retorno o atributo tam da fila que representa o tamanho
+}
 
-int f_consultar_chave_por_posicao(Fila_FIFO **f, int posicao) { return 0; }
+// funcao para consultar a chave em sua posicao
+int f_consultar_chave_por_posicao(Fila_FIFO **f, int posicao) {
+  // inicializo a variavel da posicao como 1
+  int i = 1;
 
-int f_consultar_valor_por_posicao(Fila_FIFO **f, int posicao) { return 0; }
+  // se a posicao nao existir, retorno -1 indicando erro
+  if (posicao > (*f)->tam || posicao < 1) return -1;
+
+  // percorro a fila para encontrar a posicao
+  for (Noh *p = (*f)->inicio; p != NULL; p = p->prox, ++i) {
+    if (i == posicao) return p->chave;
+    // retorno a chave quando a posicao for encontrada
+  }
+
+  return -1;
+}
+
+// funcao para consultar o valor em sua posicao
+int f_consultar_valor_por_posicao(Fila_FIFO **f, int posicao) {
+  // inicializo a variavel da posicao como 1
+  int i = 1;
+
+  // se a posicao nao existir, retorno -1 indicando erro
+  if (posicao > (*f)->tam || posicao < 1) return -1;
+
+  // percorro a fila para encontrar a posicao
+  for (Noh *p = (*f)->inicio; p != NULL; p = p->prox, ++i) {
+    if (i == posicao) return p->valor;
+    // retorno o valor quando a posicao for encontrada
+  }
+
+  return -1;
+}
